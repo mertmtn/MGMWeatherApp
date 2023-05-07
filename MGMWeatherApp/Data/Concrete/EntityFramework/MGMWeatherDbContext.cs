859a8064e22881c874ad9721b30f4f1bcd218f4a
MERT METİN <mertmetin1993@gmail.com>
2023-05-07 03:30:29 +0300

    Installing Postgres provider and adding generic repository

---
+++ MGMWeatherApp/Data/Concrete/EntityFramework/MGMWeatherDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Data.Concrete.EntityFramework
+{
+    public class MGMWeatherDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json")
+                                        .Build();
+
+            var connectionString = configuration.GetConnectionString("SQLDBConnection");
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
