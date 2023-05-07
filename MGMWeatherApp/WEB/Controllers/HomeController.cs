e18cd97ffb4b13a83240184421f2e50a5cfad1b5
MERT METİN <mertmetin1993@gmail.com>
2023-05-07 03:07:42 +0300

    Added Web Project

---
+++ MGMWeatherApp/WEB/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+
+namespace WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
