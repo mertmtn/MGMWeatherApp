d1f878039c1b6993e9299ca85979d982ef0246f0
ilker atabay <atabayilker@gmail.com>
2023-05-25 03:08:47 +0300

    feat: Stadium add page is altered to have minimal inputs. Stadium edit page is implemented.

--- MGMWeatherApp/WEB/Controllers/StadiumController.cs
+++ MGMWeatherApp/WEB/Controllers/StadiumController.cs
@@ -89,22 +89,47 @@ namespace WEB.Controllers
         ///TODO: Modal olacaksa partial view olabilir.
         public IActionResult Edit()
         {
-            return View();
+            var vm = new StadiumEditViewModel()
+            {
+                WeatherTypes = new List<WeatherType>()
+                {
+                    new() { Id = 1, Type = "Açık", Description = "Açık" },
+                    new() { Id = 2, Type = "Parçalı Bulutlu", Description = "Parçalı Bulutlu" },
+                    new() { Id = 3, Type = "Az Bulutlu", Description = "Az Bulutlu" },
+                    new() { Id = 4, Type = "Çok Bulutlu", Description = "Çok Bulutlu" },
+                    new() { Id = 5, Type = "Pus", Description = "Pus" },
+                    new() { Id = 6, Type = "Sisli", Description = "Sisli" },
+                    new() { Id = 7, Type = "Duman", Description = "Duman" },
+                    new() { Id = 8, Type = "Hafif Yağmurlu", Description = "Hafif Yağmurlu" },
+                    new() { Id = 9, Type = "Yağmurlu", Description = "Yağmurlu" },
+                    new() { Id = 10, Type = "Kuvvetli Yağmurlu", Description = "Kuvvetli Yağmurlu" },
+                    new() { Id = 11, Type = "Karla Karışık Yağmurlu", Description = "Karla Karışık Yağmurlu" },
+                    new() { Id = 12, Type = "Rüzgarlı", Description = "Rüzgarlı" },
+                    new() { Id = 13, Type = "Hafif Kar Yağışlı", Description = "Hafif Kar Yağışlı" },
+                    new() { Id = 14, Type = "Kar Yağışlı", Description = "Kar Yağışlı" },
+                    new() { Id = 15, Type = "Yoğun Kar Yağışlı", Description = "Yoğun Kar Yağışlı" },
+                    new() { Id = 16, Type = "Kuvvetli Gökgürültülü Sağanak Yağışlı", Description = "Kuvvetli Gökgürültülü Sağanak Yağışlı" },
+                    new() { Id = 17, Type = "Yer Yer Sağanak Yağışlı", Description = "Yer Yer Sağanak Yağışlı" },
+                    new() { Id = 18, Type = "Dolu", Description = "Dolu" },
+                    new() { Id = 19, Type = "Gökgürültülü Sağanak Yağışlı", Description = "Gökgürültülü Sağanak Yağışlı" }
+                }
+            };
+            return View(vm);
         }
 
         ///TODO: Modal olacaksa redirect belirtilebilir.
         [HttpPost]
-        public IActionResult Edit(StadiumCreateOrUpdateViewModel stadiumViewModel)
+        public IActionResult Edit([FromBody] StadiumEditRequest request)
         {
             try
             {
-                var result = _stadiumService.Add(new Entities.Concrete.Stadium
-                {
-                    Id = stadiumViewModel.Id,
-                    Name = stadiumViewModel.Name,
-                    CityId = stadiumViewModel.CityId
-                });
-                return Json(result.Message);
+                //var result = _stadiumService.Add(new Entities.Concrete.Stadium
+                //{
+                //    Id = stadiumViewModel.Id,
+                //    Name = stadiumViewModel.Name,
+                //    CityId = stadiumViewModel.CityId
+                //});
+                return Json(new { });
             }
             catch (Exception ex)
             {
---
+++ MGMWeatherApp/WEB/ViewModels/StadiumEditViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Concrete;
+
+namespace WEB.ViewModels
+{
+    public class StadiumEditViewModel
+    {
+        public List<WeatherType> WeatherTypes { get; set; }
+
+        public StadiumEditViewModel()
+        {
+            this.WeatherTypes = new List<WeatherType>();
+        }
+    }
+
+    public class StadiumEditRequest
+    {
+        public List<StadiumEditEntity> Entities { get; set; }
+
+        public StadiumEditRequest()
+        {
+            this.Entities = new List<StadiumEditEntity>();
+        }
+    }
+
+    public class StadiumEditEntity
+    {
+        public int? Hour { get; set; }
+        public double? Temperature { get; set; }
+        public int? Precipitation { get; set; }
+        public int? SelectedWeatherTypeId { get; set; }
+    }
+}
