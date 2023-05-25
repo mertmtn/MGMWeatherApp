3c9ee756014f0c741b7a454c6b0c66795b872946
MERT METİN <mertmetin1993@gmail.com>
2023-05-26 00:19:48 +0300

    Model binding for stadium create and fill dropdown list

--- MGMWeatherApp/WEB/Controllers/StadiumController.cs
+++ MGMWeatherApp/WEB/Controllers/StadiumController.cs
@@ -8,10 +8,12 @@ namespace WEB.Controllers
     public class StadiumController : Controller
     {
         private readonly IStadiumService _stadiumService;
+        private readonly IFihristService _fihristService;
 
-        public StadiumController(IStadiumService stadiumService)
+        public StadiumController(IStadiumService stadiumService, IFihristService fihristService)
         {
             _stadiumService = stadiumService;
+            _fihristService = fihristService;
         }
 
         public IActionResult Index()
@@ -23,67 +25,36 @@ namespace WEB.Controllers
         ///TODO: Modal olacaksa partial view olabilir.
         public IActionResult Create()
         {
-            // TODO: Burayı DB'den beslemek gerekecek.
-
             var vm = new StadiumCreateViewModel()
             {
                 MeasureDate = DateTime.Now.Date,
-                Cities = new List<City>()
-                {
-                    new() { Id = 1, Name = "Şehir 1" },
-                    new() { Id = 2, Name = "Şehir 2" },
-                    new() { Id = 3, Name = "Şehir 3" },
-                    new() { Id = 4, Name = "Şehir 4" },
-                    new() { Id = 5, Name = "Şehir 5" },
-                    new() { Id = 6, Name = "Şehir 6" },
-                },
-                WeatherTypes = new List<WeatherType>()
-                { 
-                    new() { Id = 1, Type = "Açık", Description = "Açık" },
-                    new() { Id = 2, Type = "Parçalı Bulutlu", Description = "Parçalı Bulutlu" },
-                    new() { Id = 3, Type = "Az Bulutlu", Description = "Az Bulutlu" },
-                    new() { Id = 4, Type = "Çok Bulutlu", Description = "Çok Bulutlu" },
-                    new() { Id = 5, Type = "Pus", Description = "Pus" },
-                    new() { Id = 6, Type = "Sisli", Description = "Sisli" },
-                    new() { Id = 7, Type = "Duman", Description = "Duman" },
-                    new() { Id = 8, Type = "Hafif Yağmurlu", Description = "Hafif Yağmurlu" },
-                    new() { Id = 9, Type = "Yağmurlu", Description = "Yağmurlu" },
-                    new() { Id = 10, Type = "Kuvvetli Yağmurlu", Description = "Kuvvetli Yağmurlu" },
-                    new() { Id = 11, Type = "Karla Karışık Yağmurlu", Description = "Karla Karışık Yağmurlu" },
-                    new() { Id = 12, Type = "Rüzgarlı", Description = "Rüzgarlı" },
-                    new() { Id = 13, Type = "Hafif Kar Yağışlı", Description = "Hafif Kar Yağışlı" },
-                    new() { Id = 14, Type = "Kar Yağışlı", Description = "Kar Yağışlı" },
-                    new() { Id = 15, Type = "Yoğun Kar Yağışlı", Description = "Yoğun Kar Yağışlı" },
-                    new() { Id = 16, Type = "Kuvvetli Gökgürültülü Sağanak Yağışlı", Description = "Kuvvetli Gökgürültülü Sağanak Yağışlı" },
-                    new() { Id = 17, Type = "Yer Yer Sağanak Yağışlı", Description = "Yer Yer Sağanak Yağışlı" },
-                    new() { Id = 18, Type = "Dolu", Description = "Dolu" },
-                    new() { Id = 19, Type = "Gökgürültülü Sağanak Yağışlı", Description = "Gökgürültülü Sağanak Yağışlı" }
-                }
+                Cities = _fihristService.GetAllCity().Data.Select(x => new City { Id = x.Value, Name = x.Text }).ToList(),
+                WeatherTypes = _fihristService.GetAllWeatherType().Data.Select(x => new WeatherType { Id = x.Value, Type = x.Text }).ToList()
             };
 
             return View(vm);
         }
-         
+
         ///TODO: Modal olacaksa redirect belirtilebilir.
         [HttpPost]
         public IActionResult Create(StadiumCreateViewModel stadiumViewModel)
         {
             try
             {
-                // TODO: ekrandan gelen verilerin kaydedilmesi.
-                //var result = _stadiumService.Add(new Entities.Concrete.Stadium
-                //{
-                //    Id = stadiumViewModel.Id,
-                //    Name = stadiumViewModel.StadiumDetails?.Name ?? "",
-                //    CityId = stadiumViewModel.SelectedCityId
-                //});
-                return RedirectToAction("Index", "Home");
+                var result = _stadiumService.Add(new Stadium
+                {
+                    Name = stadiumViewModel.Name ?? "",
+                    CityId = stadiumViewModel.SelectedCityId
+                });
+                return RedirectToAction("Index", "Stadium");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                return RedirectToAction("Index", "Home");
-                throw;
-            } 
+                stadiumViewModel.Cities = _fihristService.GetAllCity().Data.Select(x => new City { Id = x.Value, Name = x.Text }).ToList();
+                stadiumViewModel.WeatherTypes = _fihristService.GetAllWeatherType().Data.Select(x => new WeatherType { Id = x.Value, Type = x.Text }).ToList();
+
+                return View(stadiumViewModel);
+            }
         }
 
         ///TODO: Modal olacaksa partial view olabilir.
@@ -91,28 +62,7 @@ namespace WEB.Controllers
         {
             var vm = new StadiumEditViewModel()
             {
-                WeatherTypes = new List<WeatherType>()
-                {
-                    new() { Id = 1, Type = "Açık", Description = "Açık" },
-                    new() { Id = 2, Type = "Parçalı Bulutlu", Description = "Parçalı Bulutlu" },
-                    new() { Id = 3, Type = "Az Bulutlu", Description = "Az Bulutlu" },
-                    new() { Id = 4, Type = "Çok Bulutlu", Description = "Çok Bulutlu" },
-                    new() { Id = 5, Type = "Pus", Description = "Pus" },
-                    new() { Id = 6, Type = "Sisli", Description = "Sisli" },
-                    new() { Id = 7, Type = "Duman", Description = "Duman" },
-                    new() { Id = 8, Type = "Hafif Yağmurlu", Description = "Hafif Yağmurlu" },
-                    new() { Id = 9, Type = "Yağmurlu", Description = "Yağmurlu" },
-                    new() { Id = 10, Type = "Kuvvetli Yağmurlu", Description = "Kuvvetli Yağmurlu" },
-                    new() { Id = 11, Type = "Karla Karışık Yağmurlu", Description = "Karla Karışık Yağmurlu" },
-                    new() { Id = 12, Type = "Rüzgarlı", Description = "Rüzgarlı" },
-                    new() { Id = 13, Type = "Hafif Kar Yağışlı", Description = "Hafif Kar Yağışlı" },
-                    new() { Id = 14, Type = "Kar Yağışlı", Description = "Kar Yağışlı" },
-                    new() { Id = 15, Type = "Yoğun Kar Yağışlı", Description = "Yoğun Kar Yağışlı" },
-                    new() { Id = 16, Type = "Kuvvetli Gökgürültülü Sağanak Yağışlı", Description = "Kuvvetli Gökgürültülü Sağanak Yağışlı" },
-                    new() { Id = 17, Type = "Yer Yer Sağanak Yağışlı", Description = "Yer Yer Sağanak Yağışlı" },
-                    new() { Id = 18, Type = "Dolu", Description = "Dolu" },
-                    new() { Id = 19, Type = "Gökgürültülü Sağanak Yağışlı", Description = "Gökgürültülü Sağanak Yağışlı" }
-                }
+                WeatherTypes = _fihristService.GetAllWeatherType().Data.Select(x => new WeatherType { Id = x.Value, Type = x.Text }).ToList()
             };
             return View(vm);
         }
@@ -123,11 +73,11 @@ namespace WEB.Controllers
         {
             try
             {
-                //var result = _stadiumService.Add(new Entities.Concrete.Stadium
+                //var result = _stadiumService.Add(new Stadium
                 //{
-                //    Id = stadiumViewModel.Id,
-                //    Name = stadiumViewModel.Name,
-                //    CityId = stadiumViewModel.CityId
+                //    Id = request.Id,
+                //    Name = request.Name,
+                //    CityId = request.CityId
                 //});
                 return Json(new { });
             }
