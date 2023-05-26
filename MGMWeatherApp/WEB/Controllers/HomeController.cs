042abd6278833fb12148a9e6b7843f4d18cf4a3f
ilker atabay <atabayilker@gmail.com>
2023-05-26 12:28:04 +0300

    feat: Added home screen details.

--- MGMWeatherApp/WEB/Controllers/HomeController.cs
+++ MGMWeatherApp/WEB/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using System.Xml.Linq;
+using WEB.ViewModels;
 using static System.Net.WebRequestMethods;
 
 namespace WEB.Controllers
@@ -15,7 +16,88 @@ namespace WEB.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new HomeIndexViewModel()
+            {
+                Cities = new()
+                {
+                    new City()
+                    {
+                        Id = 1,
+                        Name = "Şehir 1",
+                        Districts = new()
+                        {
+                            new District()
+                            {
+                                Id = 1,
+                                Name = "Şehir 1 ilçe 1"
+                            },
+                            new District()
+                            {
+                                Id = 2,
+                                Name = "Şehir 1 ilçe 2"
+                            }
+                        }
+                    },
+                    new City()
+                    {
+                        Id = 2,
+                        Name = "Şehir 2",
+                        Districts = new()
+                        {
+                            new District()
+                            {
+                                Id = 3,
+                                Name = "Şehir 2 ilçe 1"
+                            },
+                            new District()
+                            {
+                                Id = 4,
+                                Name = "Şehir 2 ilçe 2"
+                            }
+                        }
+                    }
+                }
+            };
+
+            return View(vm);
+        }
+
+        public JsonResult GetWeatherByCityAndDistrict(int? cityId, int? districtId)
+        {
+            var vm = new WeatherDetailViewModel()
+            {
+                Latitude = 39.9727,
+                Longitude = 32.8637,
+                WeatherDetails = new()
+                {
+                    new WeatherDetailPerHourGap()
+                    {
+                        Day = "Çarşamba",
+                        HourGap = "13:00-14:00",
+                        WeatherTypeName = "Güneşli",
+                        Temperature = 13,
+                        TemperatureFeelsLike = 14,
+                        Humidity = 86,
+                        WindDirection = "Kuzey Batı",
+                        AverageWindSpeed = 6,
+                        MaxWindSpeed = 12
+                    },
+                    new WeatherDetailPerHourGap()
+                    {
+                        Day = "Çarşamba",
+                        HourGap = "14:00-16:00",
+                        WeatherTypeName = "Güneşli",
+                        Temperature = 17,
+                        TemperatureFeelsLike = 18,
+                        Humidity = 82,
+                        WindDirection = "Kuzey",
+                        AverageWindSpeed = 8,
+                        MaxWindSpeed = 11
+                    }
+                }
+            };
+
+            return Json(vm);
         }
     }
 }
---
+++ MGMWeatherApp/WEB/ViewModels/WeatherDetailViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace WEB.ViewModels
+{
+    public class WeatherDetailViewModel
+    {
+        public List<WeatherDetailPerHourGap> WeatherDetails { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public WeatherDetailViewModel()
+        {
+            this.WeatherDetails = new List<WeatherDetailPerHourGap>();
+        }
+    }
+
+    public class WeatherDetailPerHourGap
+    {
+        public string? HourGap { get; set; }
+        public string? Day { get; set; }
+        public string? WeatherTypeName { get; set; }
+        public int Temperature { get; set; }
+        public int TemperatureFeelsLike { get; set; }
+        public int Humidity { get; set; }
+        public string? WindDirection { get; set; }
+        public int AverageWindSpeed { get; set; }
+        public int MaxWindSpeed { get; set; }
+    }
+}
